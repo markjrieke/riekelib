3cb125a567e8527c2faeaf8472d5e94f6f5eb90b
Mark Rieke <markjrieke@gmail.com>
2023-07-17 16:51:05 -0500

    remove globals

--- R/tidyverse_extensions.R
+++ R/tidyverse_extensions.R
@@ -9,6 +9,7 @@
 #' @importFrom dplyr arrange
 #' @importFrom dplyr desc
 #' @importFrom dplyr pull
+#' @importFrom rlang .data
 #'
 #' @examples
 #' # create a character vector
@@ -24,12 +25,12 @@ arrange_vector <- function(x, desc = FALSE) {
   vector <- tibble::as_tibble(x)
 
   if (desc == FALSE) {
-    vector <- dplyr::arrange(vector, value)
+    vector <- dplyr::arrange(vector, .data$value)
   } else {
-    vector <- dplyr::arrange(vector, dplyr::desc(value))
+    vector <- dplyr::arrange(vector, dplyr::desc(.data$value))
   }
 
-  vector <- dplyr::pull(vector, value)
+  vector <- dplyr::pull(vector, .data$value)
 
   return(vector)
 
@@ -37,7 +38,7 @@ arrange_vector <- function(x, desc = FALSE) {
 
 #' Return the percentage each value or combination of values appear in a tibble or dataframe
 #'
-#' @param .data a tibble or dataframe
+#' @param data a tibble or dataframe
 #' @param .keep_n logical; keep or remove the `n` col created by `dplyr::count()`. Defaults to `FALSE`.
 #' @param ... parameters to pass to `dplyr::count()`
 #'
@@ -46,17 +47,18 @@ arrange_vector <- function(x, desc = FALSE) {
 #' @importFrom dplyr count
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
+#' @importFrom rlang .data
 #'
 #' @examples
 #' # return a tibble of the percentage of gear type in the `mtcars` dataset
 #' percent(mtcars, gear)
-percent <- function(.data, ..., .keep_n = FALSE) {
+percent <- function(data, ..., .keep_n = FALSE) {
 
-  df <- dplyr::count(.data, ...)
-  df <- dplyr::mutate(df, pct = n/sum(n))
+  df <- dplyr::count(data, ...)
+  df <- dplyr::mutate(df, pct = .data$n/sum(.data$n))
 
   if (.keep_n != TRUE) {
-    df <- dplyr::select(df, -n)
+    df <- dplyr::select(df, -.data$n)
   }
 
   return(df)
