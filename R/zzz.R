e84591ce47690d4c6a0c4b37091186271671d007
markjrieke <markjrieke@gmail.com>
2023-11-18 13:58:38 -0600

    update package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,14 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(...) {
 
-  cli::cli_alert_info("The following fonts must be installed for themes to function properly:")
-  cli::cli_li("Tiempos Text")
-  cli::cli_li("IBM Plex Sans")
-  cli::cli_li("Roboto Slab")
+  if (!interactive()) return()
+
+  msg <-
+    cli::cli({
+      cli::cli_alert_info("The following fonts must be installed for themes to function properly:")
+      cli::cli_li(c("Tiempos Text", "IBM Plex Sans", "Roboto Slab"))
+    })
+
+  packageStartupMessage(msg)
 
 }
 
